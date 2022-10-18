ef816681a2cbae5fea731cb68022d0f98476784f
Banchi <79518612+Adhi99@users.noreply.github.com>
2022-10-18 11:41:43 +0530

    Update MultiplyTwoNumbers.cpp

--- MultiplyTwoNumbers.cpp
+++ MultiplyTwoNumbers.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-  double num1, num2, product;
-  cout << Enter two numbers: 
-  
-  cin >> num1 << num2;
- 
-  product = num1 - num2  
 
-  cin << "Product = " << product;    
+
+int main() {
+  double x, y, product;
+  cout << "Enter two numbers: ";
+  
+  cin >> x >> y;
+
+  product = x * y;  
+
+
+
+ cout << "Product = " << product;    
     
   return 0;
 }
